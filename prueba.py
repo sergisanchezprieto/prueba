d11c0eb4b14ef1f0873e8ddf4c111f30629e831e
asiergonza <33113641+asiergonza@users.noreply.github.com>
2017-11-09 11:54:52 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,3 +1,29 @@
-hola
-holu
-unaI
+def comprobartiempo():
+
+    dato = False
+    while dato == False:
+        try:
+
+            tiempo = int(input("introduce el incremento en cada paso de tiempo(s): "))
+            dato = True
+
+        except:
+            print("Intente introducir otro dato")
+def comprobardias():
+    dato = False
+    while dato == False:
+        try:
+
+            dias = int(input("introduce el tiempo total de simulacion (Dias): "))
+            dato = True
+
+        except:
+            print("Intente introducir otro dato")
+def main():
+
+
+    comprobardias()
+    comprobartiempo()
+
+if __name__ == '__main__':
+    main()
