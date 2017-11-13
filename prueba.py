1a12eb1fe0b6f4ac2b9001471dbea106aaba5653
asiergonza <33113641+asiergonza@users.noreply.github.com>
2017-11-13 16:33:04 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
+import math
 def comprobartiempo():
 
     dato = False
@@ -9,6 +10,9 @@ def comprobartiempo():
 
         except:
             print("Intente introducir otro dato")
+
+
+    return tiempo
 def comprobardias():
     dato = False
     while dato == False:
@@ -19,11 +23,41 @@ def comprobardias():
 
         except:
             print("Intente introducir otro dato")
+
+
+
+    return dias
+def aceleracion(fuerza):
+    acel = [fuerza[0]/7.348e+22 , fuerza[1]/7.348e+22]
+    return acel
+def calculartiempodia(tiempo):
+    tiemp = 86400 / tiempo
+    return tiemp
+def Dvelocidad():
+
+    Dvel = aceleracion * tiempo
+def fuerza(vector):
+    masas = 5.9722e+24 * 7.348e+22
+    fuerza = masas /
+def calcular_radio(posicion):
+    radio = posicion[0] ** 2 + posicion[1] ** 2
+    radio = math.sqrt(radio)  
+
+
+def datos():
+        fuerza_inicio = [0.0e+00 , -1.98201e+20]
+        posicion_inicio = [0.0e+00 , 3.84402e+08]
+        masa_tierra = 5.9722e+24
+        masa_luna = 7.348e+22
+        velocidad_lunar = [1023.055 , 0.0]
 def main():
+    dias = comprobardias()
+    tiempo = comprobartiempo()
+    fasesdiarias = calculartiempodia(tiempo)
 
 
-    comprobardias()
-    comprobartiempo()
+
+
 
 if __name__ == '__main__':
     main()
